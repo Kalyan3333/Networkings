199435d5f4d27bf1359f8f58cb3952b7da131d03
prudhvee kalyan amruthakavi <kalyanprudhvee333@gmail.com>
2022-07-01 15:43:03 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,8 @@
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+HOST_NAME= socket.gethostname()
+PORT= 12345
+s.listen(4)
+while True:
+    client,address =s.accept()
+    print(address)
